eb8092d6eb647c9712e5972305764bbd69e27d0a
dh-r-uv <Dhruv.Kothari@iitb.ac.in>
2023-11-06 13:42:39 +0530

    handled negative offset

--- non-pipelined/converter.py
+++ non-pipelined/converter.py
@@ -1,11 +1,14 @@
 
-def bintodec(bin):  #converting binary to int
-    return int(bin, 2)
+def bintodec(bin):  #converting binary to int, making it to be signed
+    if(bin[0] == '1'):  #negative int if 1 is the first bit 
+        val = int(bin[1:]) - 1<<16 
+    else:
+        val = int(bin, 2)    
+    return val
 
 def hextobin(hex):
     hex_int = int(hex[2:], 16)  
-# Convert the integer to binary using bitwise operators  
-    bin_str = format(hex_int, 'b')  
+    bin_str = format(hex_int, 'b')  # Convert the integer to binary using bitwise operators  
     if(len(bin_str)<32):    #sign extend to 32 bits
             bin_str = '0' *(32-len(bin_str)) + bin_str
     return bin_str
